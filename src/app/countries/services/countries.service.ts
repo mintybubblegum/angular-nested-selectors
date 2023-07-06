2894db13e506532c79b3e5143ce19ba368468a80
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-06 13:26:58 +0200

    get countries by region - exercise 264

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,17 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country.interfaces';
+import { HttpClient } from '@angular/common/http';
+
+import { Region, SmallCountry } from '../interfaces/country.interfaces';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
   private _regions: Region[] = [ Region.Africa, Region.Americas, Region.Asia, Region.Europe, Region.Oceania ];
 
-  constructor() { }
+  constructor( 
+    private http: HttpClient
+  ) { }
 
   get regions(): Region[] {
     return [ ...this._regions ];
+  }
+
+  getCountriesByRegion( region : Region ): Observable<SmallCountry[]> { //hemos exportado una interfaz nueva y personalizada sólo con la información que queremos extraer de la API
+    //si necesito hacer peticiones http tengo que importarlas desde HttpClient
+
+    if ( !region ) return of([]);
+
+    const url: string = `${ this.baseUrl }/region/${ region }?fields=cca3,name,borders`;
+
+    return this.http.get<SmallCountry[]>(url)
+      .pipe(
+        tap( response => console.log({ response }) )
+      )
   }
 
 }
