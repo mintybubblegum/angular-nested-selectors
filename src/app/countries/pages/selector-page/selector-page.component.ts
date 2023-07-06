6b9b0125cb9b3104a415da25791920f4753603a5
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-06 17:06:26 +0200

    third nested selector - borders - exercise 267

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { switchMap, tap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
@@ -14,11 +14,12 @@ import { Region, SmallCountry } from '../../interfaces/country.interfaces';
 export class SelectorPageComponent implements OnInit {
 
   public countriesByRegion: SmallCountry[] = [];
+  public borders: string[] = [];
 
   public myForm: FormGroup = this.fb.group({
-    region: ['', Validators.required ],
+    region : ['', Validators.required ],
     country: ['', Validators.required ],
-    borders: ['', Validators.required ],
+    border : ['', Validators.required ],
   })
 
   constructor(
@@ -28,7 +29,7 @@ export class SelectorPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.onRegionChanged();
-    
+    this.onCountryChanged();
   }
 
   get regions(): Region[] {
@@ -39,10 +40,23 @@ export class SelectorPageComponent implements OnInit {
     this.myForm.get('region')!.valueChanges
       .pipe(
         tap( () => this.myForm.get('country')!.setValue('')), //para hacer que el campo de país se reestablezca a --Select country-- cuando ya se ha hecho una busqueda. Sin esto, el campo aparece en blanco.
+        tap( () => this.borders = [] ),
         switchMap( region => this.countriesService.getCountriesByRegion(region) ),
       )
       .subscribe( countries => {
         this.countriesByRegion = countries;
+      });
+  }
+
+  onCountryChanged():void {
+    this.myForm.get('country')!.valueChanges
+      .pipe(
+        tap( () => this.myForm.get('border')!.setValue('')), //para hacer que el campo de país se reestablezca a --Select country-- cuando ya se ha hecho una busqueda. Sin esto, el campo aparece en blanco.
+        filter( (value: string) => value.length > 0 ),
+        switchMap( (alphaCode) => this.countriesService.getCountryByAlphaCode(alphaCode) ),
+      )
+      .subscribe( country => {
+        this.borders = country.borders;
       });
   }
 
